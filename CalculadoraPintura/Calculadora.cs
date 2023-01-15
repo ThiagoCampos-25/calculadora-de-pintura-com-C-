de11340de3eee26c1b6663f63ee5692b24e92800
ThiagoCampos-25 <thiago.f.f.l@hotmail.com>
2023-01-14 21:40:30 -0300

    first commit

---
+++ CalculadoraPintura/Calculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPintura
+{
+    internal class Calculadora
+    {
+        private double areaParedes;
+
+        private double areaTeto;
+
+        public double CalcularAreaParedes(double largura, double profundidade, double altura)
+        {
+            areaParedes = 2 * (largura + profundidade) * altura;
+            return areaParedes;
+        }
+
+        public double CalcularAreaTeto(double largura, double profundidade)
+        {
+            areaTeto = largura * profundidade;
+            return areaTeto;
+        }
+
+        public double CalcularLitragemNecessaria()
+        {
+            return (areaParedes + areaTeto) / 10;
+        }
+    }
+}
---
+++ CalculadoraPintura/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPintura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é largura do Cômodo");
+            double largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quala a profundidade");
+            double profundidade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área das Paredes é: ");
+            const double Altura = 2.9;
+
+
+            Calculadora calculadora = new Calculadora();
+            Console.WriteLine(calculadora.CalcularAreaParedes(largura, profundidade, Altura));
+
+            Console.WriteLine("A área do teto: ");
+            Console.WriteLine(calculadora.CalcularAreaTeto(largura, profundidade));
+
+            Console.WriteLine("A litragem de tinta necessária é: ");
+            Console.WriteLine(calculadora.CalcularLitragemNecessaria());
+            Console.ReadLine();
+        }
+    }
+}
